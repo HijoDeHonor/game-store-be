a648ab22ea2561b1ada0f5f9f9ffde8d237e23b5
NicoGrandoli <163182208+HijoDeHonor@users.noreply.github.com>
2024-06-18 09:11:24 -0300

    feat:  add centralized error handler.
    Added a centralized error handler to streamline error management. Refactored existing try-catch blocks to use the new error handler for improved code maintainability and readability.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import { errorHandler } from './errorHandler/errorhandler.js'
+import { tryCatch } from './utils/tryCatch.js'
 
 dotenv.config()
 const app = express()
@@ -9,9 +11,19 @@ app.use(express.json())
 
 const PORT = process.env.PORT ?? 4141
 
-app.get('/', (req, res) => {
-  res.send('<h1>GameStore</h1>')
-})
+app.get(
+  '/',
+  tryCatch(async (req, res) => {
+    console.log(req.body)
+    if (!req.body) {
+      throw new Error('req body doesnt exits')
+    }
+
+    res.send('<h1>GameStore</h1>')
+  })
+)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`)
