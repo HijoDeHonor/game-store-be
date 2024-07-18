5d5349009ea651e060ee4d09cd067a27a890ada7
NicoGrandoli <163182208+HijoDeHonor@users.noreply.github.com>
2024-07-18 15:19:38 -0300

    refactor: prevent duplicate items in user inventory
    - Refactored InventoryRepository to check if the user already has the item before adding it to the inventory
    - Implemented endpoint to add items to the user's inventory

--- src/inventory/inventoryRepository.js
+++ src/inventory/inventoryRepository.js
@@ -47,8 +47,10 @@ export class InventoryRepository {
   async addItemToUser (userName, item, quantity) {
     try {
       const rows = await this.mySQLConnection.executeQuery(
-      `INSERT INTO user_items (user_userName, item_Name, Quantity)
-       VALUES (?, ?, ?);`,
+      ` INSERT INTO user_items (user_userName, item_Name, Quantity)
+        VALUES (?, ?, ?)
+        ON DUPLICATE KEY UPDATE Quantity = Quantity + VALUES(Quantity);
+      `,
       [userName, item, quantity]
       );
       if (rows.length === 0) {
