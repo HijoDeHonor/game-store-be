c0b2253a662e29d1f5a00d3d91973340d657eeb9
NicoGrandoli <163182208+HijoDeHonor@users.noreply.github.com>
2024-08-15 14:30:31 -0300

    Remove directory ErrorTypes

--- src/errors/ErrorTypes/alreadyExistError.js
+++
@@ -1,9 +0,0 @@
-import { ALREADY_EXIST_ERROR } from '../../utils/textConstants.js';
-
-export class AlreadyExistError extends Error {
-  constructor (message, entity) {
-    super(message);
-    this.name = ALREADY_EXIST_ERROR;
-    this.entity = entity;
-  }
-}
