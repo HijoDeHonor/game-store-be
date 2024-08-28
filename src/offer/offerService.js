09e337e9c995a839ced2366653eb9f4d6b2b3496
NicoGrandoli <163182208+HijoDeHonor@users.noreply.github.com>
2024-08-28 16:37:10 -0300

    removed unnecessary logs

--- src/offer/offerService.js
+++ src/offer/offerService.js
@@ -42,16 +42,13 @@ export class OfferService {
     }
 
     const offer = await this.offerRepository.getOffer(id);
-    console.log(offer);
     const { offerItems, requestItems } = offer[0];
 
     if (!await this.userRepository.exist(userNameTrader)) {
       throw new DoesNotExistError(DOES_NOT_EXIST, USER_TRADER);
     }
     const userTraderItems = await this.inventoryRepository.getQuantities(userNameTrader, requestItems);
-    console.log(userTraderItems);
     const hasEnoght = this.compareItems(userTraderItems, requestItems);
-    console.log(hasEnoght);
     if (!hasEnoght) {
       throw new InvalidDataError(HAS_NOT_ENOUGH, OFFERS);
     }
